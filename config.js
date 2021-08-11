05bafc2d245483ed02754cd6ffb8b27a0c2a0517
hungkute2580 <88775606+hungkute2580@users.noreply.github.com>
2021-08-11 17:57:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    introTitle: 'Chào bé <3 !',
+    introDesc: `Em hãy trả lời những câu hỏi của anh nhé :333
+    `,
+    btnIntro: 'Dạ😙',
+    title: 'Em thích anh có phải khum ?😶😶😶',
+    desc: ' Nếu không trả lời thì là có á nha 😜😜😜 ',
+    btnYes: 'Dạ có 😶',
+    btnNo: ' Có cc nhé :v ',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    btnReply: 'Vì anh Hưng đẹp trai :333',
+    reply: 'Yêu Hưng thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Còn chờ gì nữa mà khum ib anh hử :333',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/hunghihung.com' //link mess của các bạn.
 }
